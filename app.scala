d7100f78c4bb50d5955824cdb591bd5279d5a6ea
Mike Nelson <mike@realrunners.com>
2017-10-02 17:07:37 -0600

    simplify

--- app.scala
+++ app.scala
@@ -32,34 +32,37 @@ class OneToNActor(implicit ec: ExecutionContext) extends Actor {
   }
 }
 
-object Application extends App {
+object Main extends App {
   implicit val actorSystem: ActorSystem = ActorSystem("autodta-crawler")
   implicit val executor: ExecutionContext = actorSystem.dispatcher
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val timeout: Timeout = Timeout(10.seconds)
 
+  val actor = actorSystem.actorOf(Props(new OneToNActor)
+    .withRouter(RoundRobinPool(10))
+  )
+
   def shutdown() = {
     actorSystem.terminate()
   }
 
+  def getNextStream(s: String) = {
+    (actor ? In(s)).mapTo[Stream[Out]]
+  }
+
   def run(): Future[Done] = {
     val start = System.currentTimeMillis();
-    val actor = actorSystem.actorOf(Props(new OneToNActor)
-      .withRouter(RoundRobinPool(10))
-      )
-    
-
     val concurrency = 4
     Source(
       (1 to 4).toStream.map(i => {
         println(s"1: Emitting $i")
         i.toString
       }))
-      .mapAsyncUnordered(concurrency)(s => (actor ? In(s)).mapTo[Stream[Out]])
+      .mapAsyncUnordered(concurrency)(getNextStream)
       .mapConcat(identity)
-      .mapAsyncUnordered(concurrency)(out => (actor ? In(out.x)).mapTo[Stream[Out]])
+      .mapAsyncUnordered(concurrency)(out => getNextStream(out.x))
       .mapConcat(identity)
-      .mapAsyncUnordered(concurrency)(out => (actor ? In(out.x)).mapTo[Stream[Out]])
+      .mapAsyncUnordered(concurrency)(out => getNextStream(out.x))
       .mapConcat(identity)
       .map(x => println(s"4: Received $x after ${System.currentTimeMillis() - start}"))
       .runWith(Sink.ignore)
